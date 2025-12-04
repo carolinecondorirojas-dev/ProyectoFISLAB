33674ddd2a27f29e7f8e75f4bd1ffe56dcb17517
Sharlin <branezromerosharlinvayolet03@gmail.com>
2025-12-04 09:04:34 -0400

    cambios

--- test/test_codigo.py
+++ test/test_codigo.py
@@ -1,13 +1,13 @@
 import unittest
 import cod
 
-class TestCod(unittest.TestCase):
-    def test_suma(self):
-        self.assertEqual(cod.suma(2, 3), 5) 
+def test_suma():
+    assert cod.suma(2, 3) == 5
 
-    def test_multiplicacion(self):
-        self.assertEqual(cod.multiplicacion(2, 3), 6)  
+def test_multiplicacion():
+    assert cod.multiplicacion(2, 3) == 6
 
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == '_main_':
+    test_suma()
+    test_multiplicacion()
+    print("Todas las pruebas pasaron.")
